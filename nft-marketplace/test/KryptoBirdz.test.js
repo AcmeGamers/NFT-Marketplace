31e7ed49368a76e5854e310f95b9eb63190d0d7f
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-04-11 06:07:18 +0500

    getting new list of abis and adding minting test

--- nft-marketplace/test/KryptoBirdz.test.js
+++ nft-marketplace/test/KryptoBirdz.test.js
@@ -1,16 +1,39 @@
-const assert = require("chai");
-const KryptoBirdz = require("./Kryptobird");
+const chai = require("chai");
+var assert = chai.assert;
+const KryptoBirdz = artifacts.require("./Kryptobird");
 
 // Checking for Chai
 require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("Kryptobird", (accounts) => {
+contract("Kryptobird", async (accounts) => {
+  let k = await KryptoBirdz.deployed();
   describe("Kryptobird", () => {
     it("Deployed Sucessfully", async () => {
-      const k = await KryptoBirdz.deployed();
-      assert.equal(k.address, accounts[0]);
+      let address = k.address;
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+      assert.notEqual(address, 0x0);
+    });
+    it("Has Name", async () => {
+      let name = await k.name();
+      assert.equal(name, "KryptoBird");
+    });
+    it("Has Symbol", async () => {
+      let symbol = await k.symbol();
+      assert.equal(symbol, "KBIRDZ");
+    });
+  });
+
+  describe("minting", async () => {
+    it("Creates a new token", async () => {
+      const mint = await k.mint("1"),
+        totalSupply = await k.totalSupply;
+
+      // Success Region
+      console.log(`Minting: ${mint} \nTotal Supply = ${totalSupply}`);
     });
   });
 });
