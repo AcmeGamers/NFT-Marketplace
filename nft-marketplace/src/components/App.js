2d52833ba14a68c2deef7383fda919ef10a32081
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2022-04-15 03:50:53 +0500

    starting front-end

--- nft-marketplace/src/components/App.js
+++ nft-marketplace/src/components/App.js
@@ -16,8 +16,7 @@ import Header from "./Components/navbar/header";
 export default class Home extends Component {
   async componentDidMount() {
     await this.loadWeb3();
-    // logging account states
-    console.log(window.web3.eth.accounts);
+
     await this.loadBlockchainData();
   }
 
@@ -68,6 +67,7 @@ export default class Home extends Component {
 
       console.log("Contract Data");
       let contractData = await contract.methods.totalSupply().call();
+
       console.log("Total Supply");
       this.setState({ totalSupply: contractData });
       console.log(this.state.totalSupply);
@@ -79,22 +79,27 @@ export default class Home extends Component {
         console.log("KBirds data ");
         console.log(this.state.KBird);
       }
+    } else {
+      alert("Smart Contract Not Deployed");
     }
   }
 
   // a function that will mint tokens
-  // mintToken = async () => {
-  //   const contract = this.state.contract;
-  //   const account = this.state.account;
-  //   const amount = document.getElementById("amount").value;
-  //   console.log(amount);
-  //   contract.methods
-  //     .mint(account, amount)
-  //     .send({ from: account })
-  //     .then((result) => {
-  //       console.log(result);
-  //     });
-  // };
+  mint = async () => {
+    const contract = this.state.contract;
+    const account = this.state.account;
+
+    contract.methods
+      .mint(account)
+      .send({ from: account })
+      .once("Recipt", (error, recipt) => {
+        this.setState({ KBird: [...this.state.KBird, KryptoBird] });
+      })
+      .then((result) => {
+        console.log("Result V1");
+        console.log(result);
+      });
+  };
 
   constructor(props) {
     super(props);
@@ -122,127 +127,131 @@ export default class Home extends Component {
   }
 }
 
-// export default function Home() {
-//   // creating states to store data in accounts
-//   let [account, setAccount] = useState(""),
-//     [contract, setContract] = useState(null),
-//     [totalSupply, setTotalSupply] = useState(0),
-//     [KBird, setKBird] = useState([]),
-//     web3 = new Web3(window.web3.currentProvider);
+// React Functional Code
+/* 
+export default function Home() {
+  // creating states to store data in accounts
+  let [account, setAccount] = useState(""),
+    [contract, setContract] = useState(null),
+    [totalSupply, setTotalSupply] = useState(0),
+    [KBird, setKBird] = useState([]),
+    web3 = new Web3(window.web3.currentProvider);
 
-//   async function loadWeb3() {
-//     if (window.ethereum) {
-//       window.web3 = new Web3(window.ethereum);
-//       await window.ethereum.enable();
-//     } else if (window.web3) {
-//       window.web3 = new Web3(window.web3.currentProvider);
-//     } else {
-//       console.log(
-//         "Non-Ethereum browser detected. You should consider trying MetaMask!"
-//       );
-//     }
-//   }
+  async function loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      console.log(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+    }
+  }
 
-//   async function loadBlockchainData() {
-//     const web3 = window.web3;
-//     const acc = web3.eth.getAccounts();
-//     // saves the account in state
-//     setAccount(acc[0]);
+  async function loadBlockchainData() {
+    const web3 = window.web3;
+    const acc = web3.eth.getAccounts();
+    // saves the account in state
+    setAccount(acc[0]);
 
-//     // network id and data
-//     var networkID,
-//       getID = await web3.eth.net.getId().then((id) => {
-//         networkID = id;
-//       }),
-//       networkData = KryptoBird.networks[networkID];
+    // network id and data
+    var networkID,
+      getID = await web3.eth.net.getId().then((id) => {
+        networkID = id;
+      }),
+      networkData = KryptoBird.networks[networkID];
 
-//     console.log(`Network ID\n`);
-//     console.log(networkID);
-//     console.log(`\nNetwork Data`);
-//     console.log(networkData);
+    console.log(`Network ID\n`);
+    console.log(networkID);
+    console.log(`\nNetwork Data`);
+    console.log(networkData);
 
-//     if (networkData) {
-//       var abi = KryptoBird.abi,
-//         address = networkData.address,
-//         contract = new web3.eth.Contract(abi, address);
-//       setContract(contract);
+    if (networkData) {
+      var abi = KryptoBird.abi,
+        address = networkData.address,
+        contract = new web3.eth.Contract(abi, address);
+      setContract(contract);
 
-//       console.log(`ABI\n`);
-//       console.log(abi);
-//       console.log(`\nAddress`);
-//       console.log(address);
-//       console.log(`\nContract`);
-//       console.log(contract);
+      console.log(`ABI\n`);
+      console.log(abi);
+      console.log(`\nAddress`);
+      console.log(address);
+      console.log(`\nContract`);
+      console.log(contract);
 
-//       console.log("Contract Data");
-//       contract.methods
-//         .totalSupply()
-//         .call()
-//         .then((totalSupply) => {
-//           setTotalSupply(totalSupply);
-//           console.log("Total Supply");
-//           console.log(totalSupply);
+      console.log("Contract Data");
+      contract.methods
+        .totalSupply()
+        .call()
+        .then((totalSupply) => {
+          setTotalSupply(totalSupply);
+          console.log("Total Supply");
+          console.log(totalSupply);
 
-//           // Function to mint NFTs
-//           contract.methods
-//             .mint("https://1231")
-//             .send({ from: account })
-//             .once("transactionHash", function(hash) {
-//               console.log(hash);
-//             });
+          // Function to mint NFTs
+          contract.methods
+            .mint("https://1231")
+            .send({ from: account })
+            .once("transactionHash", function(hash) {
+              console.log(hash);
+            });
 
-//           console.log("\n\n\nNew Kbirz = ");
-//           console.log(KBird);
+          console.log("\n\n\nNew Kbirz = ");
+          console.log(KBird);
 
-//           // check contracts
-//           for (let i = 1; i <= totalSupply; i++) {
-//             console.log("a");
-//             contract.methods
-//               .kryptoBirdz(i - 1)
-//               .call()
-//               .then((result) => {
-//                 console.log("--------");
-//                 console.log(result);
-//                 setKBird([...KBird, result]);
-//               });
-//           }
-//         });
-//     }
-//   }
+          // check contracts
+          for (let i = 1; i <= totalSupply; i++) {
+            console.log("a");
+            contract.methods
+              .kryptoBirdz(i - 1)
+              .call()
+              .then((result) => {
+                console.log("--------");
+                console.log(result);
+                setKBird([...KBird, result]);
+              });
+          }
+        });
+    }
+  }
 
-//   useEffect(() => {
-//     // Adding Load Data
-//     loadWeb3();
-//     // checking blockchain data
-//     loadBlockchainData();
-//   }, []);
-//   console.log("Kbirdz");
-//   console.log(KBird);
+  useEffect(() => {
+    // Adding Load Data
+    loadWeb3();
+    // checking blockchain data
+    loadBlockchainData();
+  }, []);
+  console.log("Kbirdz");
+  console.log(KBird);
 
-//   // var accounts = new Accounts("ws://localhost:7545");
-//   // console.log(accounts);
-//   // web3.eth.net.getId().then(console.log);
-//   // web3.eth.accounts.create();
-//   // web3.eth.accounts
-//   //   .signTransaction(
-//   //     {
-//   //       to: "0x6cb1Ad9A3187689D8dC869C2C5dc67C3106247CC",
-//   //       value: "1000000000",
-//   //       gas: 2000000,
-//   //       gasPrice: "234567897654321",
-//   //       nonce: 12,
-//   //       chainId: 1,
-//   //     },
-//   //     "0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318"
-//   //   )
-//   //   .then(console.log);
+  // var accounts = new Accounts("ws://localhost:7545");
+  // console.log(accounts);
+  // web3.eth.net.getId().then(console.log);
+  // web3.eth.accounts.create();
+  // web3.eth.accounts
+  //   .signTransaction(
+  //     {
+  //       to: "0x6cb1Ad9A3187689D8dC869C2C5dc67C3106247CC",
+  //       value: "1000000000",
+  //       gas: 2000000,
+  //       gasPrice: "234567897654321",
+  //       nonce: 12,
+  //       chainId: 1,
+  //     },
+  //     "0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318"
+  //   )
+  //   .then(console.log);
 
-//   return (
-//     <>
-//       <Header account={account} />
-//       <div className="lr-padding-50px">
-//         <h1>Home Page of NFT Marketplace </h1>
-//       </div>
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Header account={account} />
+      <div className="lr-padding-50px">
+        <h1>Home Page of NFT Marketplace </h1>
+      </div>
+    </>
+  );
+}
+
+*/
